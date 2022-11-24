ff8e3df1c2f238f092417e00d373713e910d6c2b
Edmund Xin <marxin@tesla.com>
2022-11-23 18:01:01 -0800

    Add transition for modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "../Button/Button";
 import { Header, Text } from "../Text/Text";
 import { IoMdClose } from "react-icons/io";
 import "./Modal.scss";
+import FadeIn from "react-fade-in/lib/FadeIn";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
   show: boolean;
@@ -10,23 +12,46 @@ interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const Modal = ({ children, show, onClose }: ModalProps) => {
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    // cleanup this component
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
-    <div
-      className={`--modal-overlay --modal-${show}`}
-      onClick={(e) => {
-        onClose();
-      }}
-    >
-      <div className="--modal-content" onClick={(e) => e.stopPropagation()}>
-        <div className="--modal-header">
-          <Header is="h4">New application</Header>
-          <Button onClick={onClose} variant="icon">
-            <IoMdClose />
-          </Button>
-        </div>
-        <div className="--modal-body">{children}</div>
-      </div>
-    </div>
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          className={`--modal-overlay`}
+          onClick={(e) => {
+            onClose();
+          }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.07 }}
+        >
+          <div className="--modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="--modal-header">
+              <Header is="h4">New application</Header>
+              <Button onClick={onClose} variant="icon">
+                <IoMdClose />
+              </Button>
+            </div>
+            <div className="--modal-body">{children}</div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
